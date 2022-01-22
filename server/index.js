1ade52deae5685b5614e0d1ea5ba1bf9f31b1472
Alexander Raia <a.raia@gosolo.io>
2022-01-21 19:22:24 -0700

    ...

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+//has to be after routes
+app.use(express.static(__dirname + "/../build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+app.listen(3105, () => {
+  console.log(`Listening on port 3105`);
+});
