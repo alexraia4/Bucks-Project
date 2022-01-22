f6c0b4699b9d3b0ed32bf513c10208c8ca915082
Alexander Raia <a.raia@gosolo.io>
2022-01-21 19:07:29 -0700

    ...

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,7 +1,11 @@
 import "../styles/Bottom.css";
 
 function Bottom() {
-  return <div className="Bottom"> </div>;
+  return (
+    <div className="Bottom">
+      <p>©2022 Product Streamline</p>{" "}
+    </div>
+  );
 }
 
 export default Bottom;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,35 @@
 import "../styles/Contact.css";
 
 function Contact() {
-  return <div className="Contact"> </div>;
+  return (
+    <div className="Contact">
+      <div className="Contact_text">
+        <p>Tell us how we can contact you.</p>
+        <p>Name</p>
+        <textarea />
+        <div>
+          <div>
+            <p>Email</p>
+            <textarea />
+          </div>
+          <div>
+            <p>Phone Number</p>
+            <textarea />
+          </div>
+          <div>
+            <input type="checkBox" />
+            <p>Text</p>
+            <input type="checkBox" />
+            <p>Call</p>
+            <input type="checkBox" />
+            <p>Email</p>
+          </div>
+          <p>Submit</p>
+        </div>
+      </div>
+      <div className="Contact_image"></div>
+    </div>
+  );
 }
 
 export default Contact;
