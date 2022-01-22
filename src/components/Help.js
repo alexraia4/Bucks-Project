556f288f60123a8d7f990b2b35da4b48282ced1c
Alexander Raia <a.raia@gosolo.io>
2022-01-21 17:08:03 -0700

    ...

--- src/components/Help.js
+++ src/components/Help.js
@@ -7,8 +7,7 @@ function Help() {
         <p
           style={{
             color: "#009AE7",
-            fontFamily: "Arial, Helvetica, sans-serif",
-            fontSize: "2vw",
+            fontSize: "3vw",
             marginBottom: "2vh",
           }}
         >
@@ -16,25 +15,30 @@ function Help() {
         </p>
         <p
           style={{
-            color: "rgb(213, 215, 217)",
-            fontFamily: "Arial, Helvetica, sans-serif",
+            color: "rgb(177, 178, 178)",
             fontSize: "1vw",
             marginBottom: "2vh",
           }}
         >
           Tell us about what you need and we'll let you know how we can help.
         </p>
-        <div>
+        <div className="Help_get_started">
           <p>Get Started ↓</p>
         </div>
       </div>
       <div className="Help_box">
-        <p style={{ color: "#009AE7" }}>Contact Us</p>
-        <p>Text, email, or call us</p>
-        <p>+1 938-583-1859</p>
-        <p>email@gmail.com</p>
-        <p>Business Hours</p>
-        <p>8am-5pm Mountain Standard Time</p>
+        <p style={{ color: "#009AE7", fontSize: "2vw" }}>Contact Us</p>
+        <p style={{ color: "rgb(177, 178, 178)", marginBottom: "1vh" }}>
+          Text, email, or call us
+        </p>
+        <p style={{ color: "rgb(177, 178, 178)" }}>+1 938-583-1859</p>
+        <p style={{ color: "rgb(177, 178, 178)", marginBottom: "1vh" }}>
+          email@gmail.com
+        </p>
+        <p style={{ color: "#009AE7" }}>Business Hours</p>
+        <p style={{ color: "rgb(177, 178, 178)" }}>
+          8am-5pm Mountain Standard Time
+        </p>
       </div>
     </div>
   );
