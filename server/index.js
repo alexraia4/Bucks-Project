ae4d7b73c34a1f8d877c8dd13850aabcb0aebee5
Alexander Raia <a.raia@gosolo.io>
2022-01-25 08:56:24 -0700

    ...

--- server/index.js
+++ server/index.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const app = express();
 
+////////////// NODE MAILER STUFF //////////////////////////////
+// var nodemailer = require('nodemailer');
+// var transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: "alexraia4@gmail.com",
+//     pass: "PowerStars52%",
+//   },
+// });
+
+// var mailOptions = {
+//   from: "youremail@gmail.com",
+//   to: "buckmcgettigan@gmail.com",
+//   subject: "THIS IS AN AUTOMATED EMAIL SENT FROM ALEX CORP",
+//   text: "FINAL TEST",
+// };
+
+// transporter.sendMail(mailOptions, function (error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent: " + info.response);
+//   }
+// });
+///////////////////////////////////////////////////////////////
+
+const registerNewClient = (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+}
+
+
 const path = require("path");
+app.use(express.json());
+app.post('/', registerNewClient)
 
 //has to be after routes
 app.use(express.static(__dirname + "/../build"));
--- src/components/Yourself.js
+++ src/components/Yourself.js
@@ -1,56 +1,99 @@
+import { useState } from "react";
 import "../styles/Yourself.css";
-let nodemailer = require("nodemailer");
-
-const submit = () => {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "alexraia4@gmail.com",
-      pass: "PowerStars52%",
-    },
-  });
-
-  var mailOptions = {
-    from: "youremail@gmail.com",
-    to: "alexraia4@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-
-  console.log("derp");
-};
+import axios from "axios";
 
 function Want() {
+  const [product, setProduct] = useState("");
+  const [link, setLink] = useState("");
+  const [soon, setSoon] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [text, setText] = useState(false);
+  const [call, setCall] = useState(false);
+  const [emailCheck, setEmailCheck] = useState(false);
+
+  const handleProductChange = (e) => {
+    setProduct(e.target.value);
+  };
+
+  const handleLinkChange = (e) => {
+    setLink(e.target.value);
+  };
+
+  const handleSoonChange = (e) => {
+    setSoon(e.target.value);
+  };
+
+  const handleFullNameChange = (e) => {
+    setFullName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.checked);
+  };
+
+  const handleCallChange = (e) => {
+    setCall(e.target.checked);
+  };
+
+  const handleEmailCheckChange = (e) => {
+    setEmailCheck(e.target.checked);
+  };
+
+  const submit = () => {
+    const payload = {
+      product,
+      link,
+      soon,
+      fullName,
+      email,
+      phoneNumber,
+      text,
+      call,
+      emailCheck,
+    };
+
+    axios
+      .post("http://localhost:3333/", payload)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="Want">
       <p>Tell us about yourself</p>
       <p>What product do you want to source?</p>
-      <textarea />
+      <textarea onChange={handleProductChange} />
       <p>Do you have an Amazon or Alibaba link?</p>
-      <textarea />
+      <textarea onChange={handleLinkChange} />
       <p>How soon do you need samples?</p>
-      <textarea />
+      <textarea onChange={handleSoonChange} />
       <hr />
       <p>Full Name</p>
-      <textarea />
+      <textarea onChange={handleFullNameChange} />
       <p>Email</p>
-      <textarea />
+      <textarea onChange={handleEmailChange} />
       <p>Phone Number</p>
-      <textarea />
+      <textarea onChange={handlePhoneNumberChange} />
       <div>
-        <input type="checkBox" />
+        <input type="checkBox" onChange={handleTextChange} />
         <p>Text</p>
-        <input type="checkBox" />
+        <input type="checkBox" onChange={handleCallChange} />
         <p>Call</p>
-        <input type="checkBox" />
+        <input type="checkBox" onChange={handleEmailCheckChange} />
         <p>Email</p>
       </div>
       <p onClick={submit}>Submit</p>
